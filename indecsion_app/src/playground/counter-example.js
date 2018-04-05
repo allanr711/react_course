c9fa6069b172a0eaafa76072660e364af0c9b6f1
Allan Ritumban <allan@hollar.com>
2018-04-04 17:53:27 -0700

    Initial build counter state

--- indecsion_app/src/playground/counter-example.js
+++ indecsion_app/src/playground/counter-example.js
@@ -1,38 +1,25 @@
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleAddOne   = this.handleAddOne.bind(this);
     this.handleMinusOne = this.handleMinusOne.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-    this.state = {
-      count: 0
-    };
-  }
+    this.handleReset    = this.handleReset.bind(this);
+  } 
+
   handleAddOne() {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 1
-      };
-    });
+    console.log('handleAddOne');
   }
   handleMinusOne() {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count - 1
-      };
-    });
+    console.log('handleMinusOne');
   }
   handleReset() {
-    this.setState(() => {
-      return {
-        count: 0
-      };
-    });
+    console.log('handleReset');
   }
+
   render() {
     return (
       <div>
-        <h1>Count: {this.state.count}</h1>
+        <h1>Counter</h1>
         <button onClick={this.handleAddOne}>+1</button>
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>reset</button>
